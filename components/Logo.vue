e679ef181a6af0bffbdd1bfe734e4fa6aba004d8
peoray <peoray@yahoo.com>
2025-01-10 13:46:40 +0100

    fix: Replace logo and coming soon icon

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,6 +1,10 @@
 <template>
   <nuxt-link to="/" class="flex items-center">
-    <nuxt-img src="/images/icon.svg" width="32" height="32" />
-    <span class="text-3xl font-bold">Okikelabs</span>
+    <!-- <nuxt-img src="/ok.png" width="32" height="32" /> -->
+    <img src="/lab.png" alt="" width="64" height="64" />
+    <!-- <nuxt-img src="/images/icon.svg" width="32" height="32" /> -->
+    <!-- <span class="text-3xl font-bold uppercase tracking-wide ml-1"
+      >Okikelabs</span
+    > -->
   </nuxt-link>
 </template>
