26db9b43224a6ae87f055e817e35add9d26fc69a
peoray <peoray@yahoo.com>
2025-01-25 06:31:44 +0100

    fix: Open external links on new tab

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,18 +33,20 @@ export default defineNuxtConfig({
     '@nuxt/content',
     '@nuxtjs/color-mode',
   ],
-  // content: {
-  //   build: {
-  //     markdown: {
-  //       rehypePlugins: {
-  //         'rehype-external-links': {
-  //           target: '_blank',
-  //           rel: ['nofollow', 'noopener', 'noreferrer'],
-  //         },
-  //       },
-  //     },
-  //   },
-  // },
+  content: {
+    build: {
+      markdown: {
+        rehypePlugins: {
+          'rehype-external-links': {
+            options: {
+              target: '_blank',
+              rel: ['nofollow', 'noopener', 'noreferrer'],
+            },
+          },
+        },
+      },
+    },
+  },
   nitro: {
     preset: 'static',
   },
