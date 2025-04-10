43bec9b8c4a7abbf9c1d427f4968fc45c2409639
peoray <peoray@yahoo.com>
2025-04-10 03:55:11 +0100

    refactor: Update styling and structure of components, migrate to Reka UI

--- app.vue
+++ app.vue
@@ -3,9 +3,7 @@ import { usePreferredDark, useFavicon } from '@vueuse/core'
 
 const setFavicon = () => {
   const isDark = usePreferredDark()
-  const favicon = computed(() =>
-    isDark.value ? '/favicon.ico' : '/favicon-light.ico'
-  )
+  const favicon = computed(() => (isDark.value ? '/favicon.ico' : '/favicon-light.ico'))
 
   useFavicon(favicon, {
     rel: 'icon',
@@ -27,14 +25,9 @@ onMounted(() => {
 <style>
 @import './assets/css/styles.css';
 
-body {
-  background-color: #f1f5f9;
-}
-
 @font-face {
   font-family: 'Satoshi-Variable';
-  src: url('~/assets/fonts/satoshi/Fonts/Variable/Satoshi-Variable.ttf')
-    format('truetype');
+  src: url('~/assets/fonts/satoshi/Fonts/Variable/Satoshi-Variable.ttf') format('truetype');
 }
 
 html {
@@ -42,8 +35,8 @@ html {
 }
 
 body {
-  /* font-family: Satoshi-Variable; */
   @apply text-blue-tertiary;
   font-family: 'Satoshi-Variable';
+  background-color: #f1f5f9;
 }
 </style>
